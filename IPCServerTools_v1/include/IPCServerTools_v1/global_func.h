510b913d9eaa22ceca9b494fa6e87cc5b773e2ac
OceanTree1 <guangxingmrzhang@163.com>
2024-08-12 17:37:52 +0800

    类的更改

--- IPCServerTools_v1/include/IPCServerTools_v1/global_func.h
+++ IPCServerTools_v1/include/IPCServerTools_v1/global_func.h
@@ -1,7 +1,19 @@
 #include "std_library.h"
 
-class server_global_func {
+class server_global_functools_Crc {
 public:
-    static uint16_t CalculateCrc16(const uint8_t* data, size_t length, uint16_t initialValue = 0xFFFF, uint16_t polynomial = 0xA001);
+    uint16_t CalculateCrc16(const uint8_t* data, size_t length, uint16_t initialValue = 0xFFFF, uint16_t polynomial = 0xA001);
     bool CutDataforcrc16(const std::vector<uint8_t>& recBytes);
+};
+
+
+class server_global_functools_uintChange {
+public:
+    std::uint32_t vectorToUint32(const std::vector<std::uint8_t>& bytes);   // 将std::vector<uint8_t>转换为uint32_t
+    std::vector<std::uint8_t> uint16ToVector(std::uint16_t value);          // 将uint16_t转换为std::vector<uint8_t>
+    std::vector<bool> bytesToBinaryArray(const std::vector<std::uint8_t>& bytes);    // 将一组字节转换为二进制数组
+    std::vector<std::uint8_t> uint32ToVector(std::uint32_t value);           // 将uint32_t转换为std::vector<uint8_t>
+    std::uint16_t vectorToUint16(const std::vector<std::uint8_t>& bytes);    // 将std::vector<uint8_t>转换为uint16_t
+    bool isBigEndian();                                                      // 检查系统是否为大端
+    std::vector<std::uint8_t> doubleToVector(double value, bool toBigEndian = false);// 将double转换为std::vector<uint8_t>
 };
--- IPCServerTools_v1/src/CrcCalculator.cpp
+++ IPCServerTools_v1/src/CrcCalculator.cpp
@@ -3,44 +3,41 @@
 #include <cstdint>
 #include "IPCServerTools_v1/global_func.h"
 
-class CrcCalculator : server_global_func {
-public:
-    // crc16校验
-    static uint16_t CalculateCrc16(const uint8_t* data, size_t length, uint16_t initialValue = 0xFFFF, uint16_t polynomial = 0xA001) {
-        uint16_t crc = initialValue;
-        for (size_t i = 0; i < length; ++i) {
-            crc ^= data[i] << 8;
-            for (int j = 0; j < 8; ++j) {
-                if (crc & 0x8000)
-                    crc = (crc << 1) ^ polynomial;
-                else
-                    crc = crc << 1;
-            }
+// crc16校验
+uint16_t server_global_functools_Crc::CalculateCrc16(const uint8_t* data, size_t length, uint16_t initialValue = 0xFFFF, uint16_t polynomial = 0xA001) {
+    uint16_t crc = initialValue;
+    for (size_t i = 0; i < length; ++i) {
+        crc ^= data[i] << 8;
+        for (int j = 0; j < 8; ++j) {
+            if (crc & 0x8000)
+                crc = (crc << 1) ^ polynomial;
+            else
+                crc = crc << 1;
         }
-        return crc;
     }
+    return crc;
+}
 
-    bool CutDataforcrc16(const std::vector<uint8_t>& recBytes) { //CutData功能：报文切割
-        size_t DataFullLength = recBytes.size();
-        size_t crcDatasIndex = 4;
-        size_t crcDatasLength = DataFullLength - 8 - 2;
-        
-        // crc16算法调用
-        CrcCalculator crcCalculator;
-        std::vector<uint8_t> Datas_point(recBytes.begin() + crcDatasIndex, recBytes.begin() + crcDatasIndex + crcDatasLength);
-        // 调用 CRC16 计算器
-        uint16_t calculatedCrc16 = CalculateCrc16(Datas_point.data(), Datas_point.size());
-        // 提取原始数组中的 CRC16 数据
-        size_t crc16StartIndex = DataFullLength - 6; // 原始 CRC16 数据在数组中的起始索引
-        uint16_t originalCrc16 = (recBytes[crc16StartIndex] << 8) | recBytes[crc16StartIndex + 1];
-        // 比较计算得到的 CRC16 与原始数组中的 CRC16
-        if (calculatedCrc16 == originalCrc16) {
-            // 在这里使用 crc16Result，例如输出到控制台
-            std::cout << "CRC16 Result: " << std::hex << calculatedCrc16 << std::dec << std::endl;
-            return true;
-        } else {
-            std::cout << "CRC16 Check Failed!" << std::endl;
-            return false;
-        }
+bool server_global_functools_Crc::CutDataforcrc16(const std::vector<uint8_t>& recBytes) { //CutData功能：报文切割
+    size_t DataFullLength = recBytes.size();
+    size_t crcDatasIndex = 4;
+    size_t crcDatasLength = DataFullLength - 8 - 2;
+    
+    // crc16算法调用
+    server_global_functools_Crc crcCalculator;
+    std::vector<uint8_t> Datas_point(recBytes.begin() + crcDatasIndex, recBytes.begin() + crcDatasIndex + crcDatasLength);
+    // 调用 CRC16 计算器
+    uint16_t calculatedCrc16 = CalculateCrc16(Datas_point.data(), Datas_point.size());
+    // 提取原始数组中的 CRC16 数据
+    size_t crc16StartIndex = DataFullLength - 6; // 原始 CRC16 数据在数组中的起始索引
+    uint16_t originalCrc16 = (recBytes[crc16StartIndex] << 8) | recBytes[crc16StartIndex + 1];
+    // 比较计算得到的 CRC16 与原始数组中的 CRC16
+    if (calculatedCrc16 == originalCrc16) {
+        // 在这里使用 crc16Result，例如输出到控制台
+        std::cout << "CRC16 Result: " << std::hex << calculatedCrc16 << std::dec << std::endl;
+        return true;
+    } else {
+        std::cout << "CRC16 Check Failed!" << std::endl;
+        return false;
     }
-};
+}
